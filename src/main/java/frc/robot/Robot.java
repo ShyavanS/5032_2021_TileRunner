b94bb373e0cb8516363813d4ecf605efd96f6b5e
ShyavanS <35411334+ShyavanS@users.noreply.github.com>
2021-04-16 19:20:44 -0400

    First commit.

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * Simple drive program for the TileRunner training chassis.
+ */
+public class Robot extends TimedRobot {
+  // Declare motor controllers
+  private final WPI_VictorSPX leftFront = new WPI_VictorSPX(0);
+  private final WPI_VictorSPX leftRear = new WPI_VictorSPX(1);
+  private final WPI_VictorSPX rightFront = new WPI_VictorSPX(2);
+  private final WPI_VictorSPX rightRear = new WPI_VictorSPX(3);
+
+  // Group motors together for each side
+  private final SpeedControllerGroup leftVictors = new SpeedControllerGroup(leftFront, leftRear);
+  private final SpeedControllerGroup rightVictors = new SpeedControllerGroup(rightFront, rightRear);
+
+  // Declare drivetrain
+  private final DifferentialDrive robotDrive = new DifferentialDrive(leftVictors, rightVictors);
+
+  // Declare joystick
+  private final Joystick driveStick = new Joystick(0);
+
+  // Button ID for slow mode button
+  private final int R1 = 6;
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    if (driveStick.getRawButton(R1)) { // Slow mode
+      robotDrive.arcadeDrive(-driveStick.getY() * 0.5, driveStick.getX() * 0.5);
+    } else { // Normal drive
+      robotDrive.arcadeDrive(-driveStick.getY(), driveStick.getX());
+    }
+  }
+}
